e6e91b73862eb36837e85211624507cbab3b32ba
yasmin-aumeeruddy <52958137+yasmin-aumeeruddy@users.noreply.github.com>
2019-10-31 13:19:42 +0000

    Update createFile.sh

--- getting-started-guide/createFile.sh
+++ getting-started-guide/createFile.sh
@@ -1,1 +1,2 @@
 touch guide-getting-started/start/src/main/java/io/openliberty/sample/system/SystemReadinessCheck.java;
+touch guide-getting-started/start/src/main/java/io/openliberty/sample/system/SystemLivenessCheck.java;
