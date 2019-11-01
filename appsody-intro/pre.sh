77fcabe8694c5a30854d436ed16de6da854bd8e2
yasmin-aumeeruddy <52958137+yasmin-aumeeruddy@users.noreply.github.com>
2019-11-01 12:34:53 +0000

    Update pre.sh

--- appsody-intro/pre.sh
+++ appsody-intro/pre.sh
@@ -1,7 +1,7 @@
-#mkdir -p ~/workspace/kabanero-workshop/nodejs && touch workspace/kabanero-workshop/nodejs/.env
-#echo "PORT=9000" >> workspace/kabanero-workshop/nodejs/.env
-#mkdir appsody-install
-# cd ~
+mkdir -p ~/workspace/kabanero-workshop/nodejs && touch workspace/kabanero-workshop/nodejs/.env
+echo "PORT=9000" >> workspace/kabanero-workshop/nodejs/.env
+mkdir appsody-install
+cd ~
 wget -P /root/appsody-install/ "https://github.com/appsody/appsody/releases/download/0.4.7/appsody_0.4.7_amd64.deb"
 apt install -f /root/appsody-install/appsody_0.4.7_amd64.deb
 
